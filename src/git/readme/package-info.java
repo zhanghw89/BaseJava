2c51536be0093da99b8d333c86111a7f03911331
zhanghw <1803794281@qq.com>
2015-08-15 15:51:04 +0800

    用git创建项目

---
+++ src/git/readme/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+
+/**   
+ * @Description    新建git项目
+ * 1.github上 new repository （https://github.com/zhanghw89/BaseJava.git）
+ * 2.用eclipse pull 下新建的git路径（https://github.com/zhanghw89/BaseJava.git）
+ * 3.在上次的路径下新建项目
+ * 3.将项目merge到分支，
+ * 4.就可以提交项目了
+ * @author         zhanghw@chinatelecom.com.cn
+ * @since          2015年8月15日
+ * @version        V1.0
+ */
+
+package git.readme;
