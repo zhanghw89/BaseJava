56a1897a80931e6b6ead349ea517d5f7b7cc46f4
zhanghw <1803794281@qq.com>
2015-08-15 15:21:38 +0800

    面向对象思想

---
+++ src/com/zhangshiwen/oo/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+
+/**   
+ * @Description    面向对象的基础知识
+ * @author         zhanghw@chinatelecom.com.cn
+ * @since          2015年8月15日
+ * @version        V1.0
+ */
+
+package com.zhangshiwen.oo;
