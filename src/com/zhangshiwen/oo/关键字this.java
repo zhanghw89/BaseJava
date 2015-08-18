c0add739c43495ac0c85fba19ec20ec5e79d18b1
zhanghw <1803794281@qq.com>
2015-08-18 18:04:01 +0800

    new

---
+++ src/com/zhangshiwen/oo/关键字this.java
@@ -0,0 +1,8 @@
+package com.zhangshiwen.oo;
+
+public class 关键字this {
+	/**
+	 * this:代表当前函数所属对象的引用，（预先给对象留出的位置）
+	 */
+
+}
---
+++ src/com/zhangshiwen/oo/匿名对象.java
@@ -0,0 +1,13 @@
+package com.zhangshiwen.oo;
+
+public class 匿名对象 {
+	/*
+	 * Car a = new Car();
+	 * a.color ="red";
+	 * 上边的可以表示为(匿名对象)
+	 * new Car().color ="red";
+	 * 用法1：当对象的方法只调用一次的话可以用匿名对象。但是如果对象有多个成员调用，则必须给对象起个名字。
+	 * 用法2：作为函数运行时的实体参数（可以减少内存引用）
+	 */
+
+}
